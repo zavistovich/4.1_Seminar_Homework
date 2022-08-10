ebce578bc1695be47702852efba0835c8a5e1b80
serge <szavistovich@gmail.com>
2022-08-10 15:04:22 +0300

    release v1

---
+++ 05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+Console.Clear();
+
+Console.Write("Enter extreme value N: ");
+int a = int.Parse(Console.ReadLine());
+int b = -a;
+
+while (b <= a)
+{
+    Console.Write(b + "   "); 
+    b++;
+}
